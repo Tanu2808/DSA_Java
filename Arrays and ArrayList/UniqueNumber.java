c53182c9c2864a5ed1a5d01bae62b8f9ff165e43
Tanush Vaish <tanushv28@gmail.com>
2025-12-13 00:29:10 +0530

    New Start

--- Arrays and ArrayList/UniqueNumber.java
+++
@@ -1,8 +0,0 @@
-public class UniqueNumber {
-    public int findUnique(int[] arr) {
-        // code here
-        int result = arr[0];
-        for (int i = 1;i < arr.length;i++) result = result ^ arr[i];
-        return result;
-    }
-}
