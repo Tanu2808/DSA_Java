5b54c76e46b1d98e66bf7e64af9cd513534819c6
Tanush Vaish <tanushv28@gmail.com>
2025-12-16 23:44:29 +0530

    Selection Sort

---
+++ Sorting/Selection.java
@@ -0,0 +1,17 @@
+public class Selection {
+
+    public void selection(int[] arr)
+    {
+        
+        for (int i = 0;i < arr.length;i++)
+        {
+            int minIdx = i;
+            for (int j = i + 1;j < arr.length;j++)
+            {
+                if (arr[j] < arr[minIdx]) minIdx = j;
+            }
+            arr[i] = arr[minIdx];
+            
+        }
+    }
+}
