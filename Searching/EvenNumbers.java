e1601f3bc167bf091c8a19ca4b154c19569ece96
Tanush Vaish <tanushv28@gmail.com>
2025-12-16 14:39:23 +0530

    Leetcode 1295: Even number of digits

---
+++ Searching/EvenNumbers.java
@@ -0,0 +1,20 @@
+package Searching;
+
+public class EvenNumbers {
+
+    public int findNumbers(int[] nums)
+    {
+        int count = 0;
+        for (int i: nums)
+        {
+            int digits = 0;
+            while (i != 0)
+            {
+                i /= 10;
+                digits++;
+            }
+            if (digits % 2 == 0) count++;
+        }
+        return  count;
+    }
+}
