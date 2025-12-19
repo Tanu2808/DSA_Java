71385e619d65c949a4b7432175eb981407b9c6f4
Tanush Vaish <tanushv28@gmail.com>
2025-12-20 00:33:31 +0530

    Ceiling of a given Number

---
+++ Searching/Ceiling.java
@@ -0,0 +1,21 @@
+public class Ceiling {
+    public int ceiling(int arr[], int target)
+    {
+        int result = -1;
+        if (target > arr[arr.length - 1]) return -1;
+        if (target < arr[0]) return arr[0];
+        int low = 0, high = arr.length - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) return arr[mid];
+            else if (arr[mid] > target)
+            {
+                result = arr[mid];
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return result;
+    }
+}
