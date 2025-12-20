4fc73059d2aa3d6d8f6e47193e17c10c9336d69a
Tanush Vaish <tanushv28@gmail.com>
2025-12-20 15:37:11 +0530

    LeetCode 744: Find Smallest letter Greater than target

---
+++ Searching/SmallestLetter.java
@@ -0,0 +1,16 @@
+public class SmallestLetter {
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        if (target < letters[0] || target >= letters[letters.length - 1]) return letters[0];
+
+        int low = 0, high = letters.length - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            // if (letters[mid] == target) return letters[mid];
+            if (letters[mid] <= target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return letters[low];
+    }
+}
