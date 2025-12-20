e44cf12439eca3346407a0fb83360519a35efc38
Tanush Vaish <tanushv28@gmail.com>
2025-12-20 15:08:21 +0530

    Corrected Ceiling

--- Searching/Ceiling.java
+++ Searching/Ceiling.java
@@ -1,7 +1,6 @@
 public class Ceiling {
     public int ceiling(int arr[], int target)
     {
-        int result = -1;
         if (target > arr[arr.length - 1]) return -1;
         if (target < arr[0]) return arr[0];
         int low = 0, high = arr.length - 1;
@@ -11,11 +10,10 @@ public class Ceiling {
             if (arr[mid] == target) return arr[mid];
             else if (arr[mid] > target)
             {
-                result = arr[mid];
                 high = mid - 1;
             }
             else low = mid + 1;
         }
-        return result;
+        return arr[low];
     }
 }
