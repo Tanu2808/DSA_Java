a7fa2ac4dab2a6ac7931f496d8b92b6373441f9b
Tanush Vaish <tanushv28@gmail.com>
2025-12-21 22:16:16 +0530

    Leetcode 9: Palindrome

---
+++ Basic Maths/Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int temp = x;
+        int reverse = 0;
+        while (x != 0)
+        {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+        if (temp == reverse) return true;
+        return false;
+    }
+}
