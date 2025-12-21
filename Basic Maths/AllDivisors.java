6e5ea1efb53c635839e018cd80a26a8bd29b96b4
Tanush Vaish <tanushv28@gmail.com>
2025-12-21 23:12:16 +0530

    List of Divisors of given number

---
+++ Basic Maths/AllDivisors.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllDivisors {
+
+    public List<Integer> printDivisors(int num)
+    {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < num; i++)
+        {
+            if (num / i == 0) list.add(i);
+        }
+        return list;
+    }
+}
