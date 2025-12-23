44edcc3ee6b95e19ab995861338b663b1f09bab7
Tanush Vaish <tanushv28@gmail.com>
2025-12-23 12:06:09 +0530

    LeetCode 125: Valid Palindrome

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -7,4 +7,28 @@ public class Palindrome {
         return palindrome(str, i + 1);
 
     }
+
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end)
+        {
+            if (!Character.isLetterOrDigit(s.charAt(start)))
+            {
+                start++;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(end)))
+            {
+                end--;
+            }
+            if (Character.isLetterOrDigit(s.charAt(start)) && Character.isLetterOrDigit(s.charAt(end)))
+            {
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) return false;
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
 }
