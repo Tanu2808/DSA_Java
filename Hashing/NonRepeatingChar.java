4e97f5b0e45d8a0438316d79fc24c818f00fb828
Tanush Vaish <tanushv28@gmail.com>
2025-12-27 15:03:19 +0530

    Find the first non-repeating character in a string

---
+++ Hashing/NonRepeatingChar.java
@@ -0,0 +1,20 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NonRepeatingChar {
+
+     public char nonRepeatingChar(String str)
+     {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++)
+        {
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+        }
+        for (char key : map.keySet())
+        {
+            if (map.get(key) == 1) return key;
+        }
+        return '~';
+     }
+}
