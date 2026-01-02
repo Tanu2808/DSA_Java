8d05020e6569e87416eaf3d16fd37febb73b58df
Tanush Vaish <tanushv28@gmail.com>
2026-01-02 13:46:21 +0530

    Print subarray with maximum subarray sum

--- Arrays and ArrayList/MaximumSubarray.java
+++ Arrays and ArrayList/MaximumSubarray.java
@@ -10,4 +10,32 @@ public class MaximumSubarray {
         }
         return max;
     }
+
+    public int[] printMaxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        int j = 0, k = 0;
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max) 
+            {
+                max = sum;
+                j = temp;
+                k = i;
+            }
+            if (sum < 0) {
+                sum = 0;
+                j = i + 1;
+                k = j;
+            }
+        }
+
+        int[] result = new int[k - j + 1];
+        int x = 0;
+        for (int i = j; i <= k; i++) {
+            result[x++] = nums[i];
+        }
+        return result;
+    }
 }
